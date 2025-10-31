73c2198c811f3879f8a9bdb7ba22b37c5f19c431
lutzluca0 <lutzluca0@gmail.com>
2025-10-31 04:44:55 +0100

    suggestions for taxes

--- src/main/java/com/github/lutzluca/btrbz/core/commands/TaxCommand.java
+++ src/main/java/com/github/lutzluca/btrbz/core/commands/TaxCommand.java
@@ -4,6 +4,7 @@ import com.github.lutzluca.btrbz.core.config.ConfigManager;
 import com.github.lutzluca.btrbz.utils.Notifier;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import java.util.List;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -12,11 +13,22 @@ import net.minecraft.util.Formatting;
 
 public class TaxCommand {
 
+    private static final SuggestionProvider<FabricClientCommandSource> RATE_SUGGESTIONS = (ctx, builder) -> {
+        List<String> validRates = List.of("1.0", "1.125", "1.25");
+        for (String rate : validRates) {
+            if (rate.startsWith(builder.getRemainingLowerCase())) {
+                builder.suggest(rate);
+            }
+        }
+        return builder.buildFuture();
+    };
+
     public static LiteralArgumentBuilder<FabricClientCommandSource> get() {
         return Commands.rootCommand.then(ClientCommandManager
             .literal("tax")
             .then(ClientCommandManager
                 .argument("rate", FloatArgumentType.floatArg())
+                .suggests(RATE_SUGGESTIONS)
                 .executes(ctx -> {
                     var rate = FloatArgumentType.getFloat(ctx, "rate");
 
