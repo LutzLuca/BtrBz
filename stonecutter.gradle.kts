a0c68915663c189dc98b0a90f1dae7ab196bcd15
Luca Tölle <lutzluca0@gmail.com>
2026-06-28 16:15:39 +0200

    chore: add spotless formatting

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,8 +1,22 @@
 plugins {
     id("dev.kikugie.stonecutter")
+    id("com.diffplug.spotless") version "8.7.0"
 }
 
 stonecutter active "26.1"
+
+repositories {
+    mavenCentral()
+}
+
+spotless {
+    lineEndings = com.diffplug.spotless.LineEnding.UNIX
+
+    java {
+        target("src/**/*.java")
+        eclipse().configFile(rootProject.file("formatting.xml"))
+    }
+}
 
 tasks.register("releaseMod") {
     group = "publishing"
