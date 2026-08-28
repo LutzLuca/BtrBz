93dd724250044dbb881abd8b23e42b715f64991e
raghav314 <samraghav2@gmail.com>
2026-08-29 04:39:26 +0530

    fix: round the scroll container scissor outward

---
+++ src/main/java/com/github/lutzluca/btrbz/core/widgets/ui/WidgetScissors.java
@@ -0,0 +1,23 @@
+package com.github.lutzluca.btrbz.core.widgets.ui;
+
+import net.minecraft.client.gui.navigation.ScreenRectangle;
+import net.minecraft.util.Mth;
+import org.joml.Matrix3x2fc;
+import org.joml.Vector2f;
+
+public final class WidgetScissors {
+    private WidgetScissors() {}
+
+    public static ScreenRectangle conservative(
+        Matrix3x2fc pose, int left, int top, int right, int bottom
+    ) {
+        var topLeft = pose.transformPosition(left, top, new Vector2f());
+        var bottomRight = pose.transformPosition(right, bottom, new Vector2f());
+
+        return new ScreenRectangle(
+            Mth.floor(topLeft.x),
+            Mth.floor(topLeft.y),
+            Mth.ceil(bottomRight.x - topLeft.x),
+            Mth.ceil(bottomRight.y - topLeft.y));
+    }
+}
